3ae60f85e1c78116909e01f540086c2544664507
Luiz Guilherme Oliveira da Mota <137472016+zLuizGuilhermez@users.noreply.github.com>
2023-10-02 20:41:42 -0300

    Update Atividade1.c

--- Atividade1.c
+++ Atividade1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+
+  int valores[6] = {1, 0, 5, -2, -5, 7}, soma,i;
+
+  soma = valores[0] + valores[1] + valores[5];
+
+  printf("a soma e %d", soma);
+
+  for ( i = 0; i < 6; i++){
+    if (i == 4) {
+      valores[i] = 100;
+    }
+    printf("\n Os numeros que estao na variavel e: %d", valores[i]);
+  }
+}
