8136c334e1bcd9cfdfbd3e269dbb0d629c29da1c
Luiz Guilherme Oliveira da Mota <137472016+zLuizGuilhermez@users.noreply.github.com>
2023-10-02 21:27:19 -0300

    Create Atividade2.c

---
+++ Atividade2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+  int i, valores[6],a ;
+
+  for(i = 0;i < 6;i++){
+    printf("\nDigite o seu numero");
+    scanf("%d", &valores[i]);
+  }
+  for(a = 0;a < 6; a++){
+  
+    printf("\n %d", valores[i]);
+	}
+}
