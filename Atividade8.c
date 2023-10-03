e3f36a13de31857743f6d25aed06f2a9c14aa94d
Luiz Guilherme Oliveira da Mota <137472016+zLuizGuilhermez@users.noreply.github.com>
2023-10-03 20:33:16 -0300

    Update Atividade8.c

--- Atividade8.c
+++ Atividade8.c
@@ -3,12 +3,12 @@
 int main(){
     int valores[6];
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 5; i++)
     {
         printf("Digite um numero");
         scanf("%d", &valores[i]);
     }
-    for (int i = 6; i > 0 ; i--)
+    for (int i = 4; i = 0 ; i--)
     {
         printf("\n%d", valores[i]);
     }
